215d9b1cfe9bb7b4be3c0852e42c2fa9676689e0
Kanchan Motlag <kanchan.motlag09@gmail.com>
2011-09-10 10:34:21 +0530

    installed haml-rails gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Sample::Application.routes.draw do
   get "home/index"
-   root :to => "home#index"
+  root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
