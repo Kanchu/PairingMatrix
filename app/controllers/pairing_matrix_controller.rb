4bec82b7d09120507ada5825e77a3704227f11d4
Kanchan Motlag <kanchan.motlag09@gmail.com>
2011-09-12 13:54:03 +0530

    updated

--- app/controllers/pairing_matrix_controller.rb
+++ app/controllers/pairing_matrix_controller.rb
@@ -10,11 +10,13 @@ class PairingMatrixController < ApplicationController
     @arr = Array.new
     @ar = Array.new
     @emp = Emp.find(:all)
+    @ar = @emp
     @pair = Matrix.find(:all)
     @emp.each do |c|
       @arr1 << c.id
       @arr2 << c.id
     end
+   # @ar.length = @arr1.length
     for i in (0..@arr1.length-1)
      for j in (0..@arr2.length-1)
       @arr << Matrix.find_by_EmpId1_and_EmpId2(@arr1[i],@arr2[j]).no_of_time
